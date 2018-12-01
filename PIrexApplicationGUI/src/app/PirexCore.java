0c86004556a650483714cbdb4d805b98a2025145
shazimajaz1 <shazimajaz@192.168.43.209>
2018-11-30 23:18:36 -0800

    added more code into PirexCore Class

--- PIrexApplicationGUI/src/app/PirexCore.java
+++ PIrexApplicationGUI/src/app/PirexCore.java
@@ -1,9 +1,25 @@
 package app;
 
+import javax.swing.JTabbedPane;
+
+/*
+ * This class is the core class of the entire Pirex Project
+ * The execution, flow and all other client classes are controlled from this class.
+ */
 public class PirexCore {
 
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//Create the main window
+		PirexFrame pirex = new PirexFrame();
+		
+		//Add all the components: Copied from PirexLoadingTabDriver.java class file
+		JTabbedPane jtb = new JTabbedPane();
+		jtb.add("Load Document", new PirexLoadingTab());
+		pirex.add(jtb);
+		
+		//Add functionality components
 
 	}
 
